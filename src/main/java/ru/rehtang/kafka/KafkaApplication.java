6037f549dcd951291185d4dbed2b9277422316e4
Rehtang <Warrior8584!>
2022-06-18 19:19:23 +0300

    git remote add origin https://github.com/Rehtang/kafka.git git branch -M main git push -u origin main

---
+++ src/main/java/ru/rehtang/kafka/KafkaApplication.java
@@ -0,0 +1,20 @@
+package ru.rehtang.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@EnableKafka
+@SpringBootApplication
+public class KafkaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaApplication.class, args);
+  }
+
+  @KafkaListener(topics = "msg")
+  public void msgListener(String msg) {
+    System.out.println(msg);
+  }
+}
---
+++ src/main/java/ru/rehtang/kafka/MsgController.java
@@ -0,0 +1,17 @@
+package ru.rehtang.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/msg")
+public class MsgController {
+  private KafkaTemplate<String, String> kafkaTemplate;
+
+  @PostMapping
+  public void sendOrder(String msgId, String msg) {
+    kafkaTemplate.send("msg", msgId, msg);
+  }
+}
