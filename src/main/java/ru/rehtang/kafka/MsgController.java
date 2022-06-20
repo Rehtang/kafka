200ee855686867f3077251811faf592e23dd08db
Rehtang <Warrior8584!>
2022-06-20 16:47:59 +0300

    created consumer, producer, added dto for message

--- src/main/java/ru/rehtang/kafka/MsgController.java
+++ src/main/java/ru/rehtang/kafka/MsgController.java
@@ -1,17 +1,25 @@
 package ru.rehtang.kafka;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.rehtang.kafka.dto.Address;
+import ru.rehtang.kafka.dto.UserDto;
 
 @RestController
-@RequestMapping("/msg")
+@RequestMapping("msg")
 public class MsgController {
-  private KafkaTemplate<String, String> kafkaTemplate;
+  @Autowired private KafkaTemplate<Long, UserDto> kafkaTemplate;
 
   @PostMapping
-  public void sendOrder(String msgId, String msg) {
-    kafkaTemplate.send("msg", msgId, msg);
+  public void sendMsg(Long msgId, UserDto msg) {
+    msg.setAddress(new Address("Rus", "Msk", "Bunina", 1L, 1L));
+    ListenableFuture<SendResult<Long,UserDto>> future = kafkaTemplate.send("msg", msgId, msg);
+    future.addCallback(System.out::println, System.err::println);
+    kafkaTemplate.flush();
   }
 }
